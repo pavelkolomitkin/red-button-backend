e17b6d0fd032a214114d49f12a9c434c422f0e8a
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-04-23 06:09:30 +0400

    rename some regions

--- src/Migrations/Version20190423012629.php
+++ src/Migrations/Version20190423012629.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\FetchMode;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190423004925 extends AbstractMigration
+final class Version20190423012629 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,12 +22,11 @@ final class Version20190423004925 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->connection->executeQuery("ALTER SEQUENCE region_id_seq RESTART WITH 100;");
+        $this->addSql("UPDATE region SET title = N'Москва' WHERE title = N'город Москва'");
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
     }
 }
--- src/Migrations/Version20190423013557.php
+++ src/Migrations/Version20190423013557.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\FetchMode;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190423004925 extends AbstractMigration
+final class Version20190423013557 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,12 +22,16 @@ final class Version20190423004925 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->connection->executeQuery("ALTER SEQUENCE region_id_seq RESTART WITH 100;");
+        $this->addSql("UPDATE region SET title = N'Автономная Республика Крым' WHERE title = N'Республика Крым'");
+        $this->addSql("UPDATE region SET title = N'Санкт-Петербург' WHERE title = N'город Санкт-Петербург'");
+        $this->addSql("UPDATE region SET title = N'Севастополь' WHERE title = N'город Севастополь'");
+        $this->addSql("UPDATE region SET title = N'Башкортостан' WHERE title = N'Республика Башкортостан'");
+
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
     }
 }
